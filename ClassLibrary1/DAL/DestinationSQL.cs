509ae53fbd44afc8bfe10bca7f01ef7d97e1bef3
sertoi <nguyentuongvien@yahoo.com>
2018-08-31 13:21:36 +0200

    p

--- ClassLibrary1/DAL/DestinationSQL.cs
+++ ClassLibrary1/DAL/DestinationSQL.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BoVoyage.Core.EnTity;
+using BoVoyage.Core.Entity;
 
 
 namespace BoVoyage.Core.DAL
